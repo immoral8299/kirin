42abd632185b5b006a838c6488fbae5f696de592
Immoral <nemoralni@protonmail.com>
2026-06-04 17:21:46 +0700

    feat: Fix icon position

--- Sources/MenuBarPlexClient/StatusItemController.swift
+++ Sources/MenuBarPlexClient/StatusItemController.swift
@@ -9,7 +9,7 @@ fileprivate enum StatusBarConfig {
     static let topRightScreenMargin = NSSize(width: 4, height: 4)
     static let panelSize = NSSize(width: 460, height: 520)
     static let iconFontSize: CGFloat = 11
-    static let iconVerticalOffset: CGFloat = 1
+    static let iconVerticalOffset: CGFloat = 0.5
     static let marqueeFontSize: CGFloat = 13
     static let iconTitleSpacing = "  "
     static let panelAnimationDuration: TimeInterval = 0.16
